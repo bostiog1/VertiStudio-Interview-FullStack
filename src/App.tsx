463a8db137857702e6ae0bed2ec1533f589fa34c
bostiog1 <teo.bostiog@yahoo.com>
2025-03-28 23:57:10 +0200

    Installed some packages and dark mode component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { AppRoutes } from "./frontend/routes/AppRoutes";
+import { ErrorBoundary } from "./frontend/components/ErrorBoundary";
+import { AuthListener } from "./frontend/service/AuthListener";
 
 function App() {
   return (
     <Router>
-      <AppRoutes />
+      <ErrorBoundary>
+        <AuthListener />
+        <AppRoutes />
+      </ErrorBoundary>
     </Router>
   );
 }
